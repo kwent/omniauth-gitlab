2f241ffad2a08a73f7a156c278323e4ad89995dd
Quentin Rousseau <contact@quent.in>
2022-12-09 02:52:19 -0800

    rootly

--- lib/omniauth/strategies/gitlab.rb
+++ lib/omniauth/strategies/gitlab.rb
@@ -4,9 +4,15 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class GitLab < OmniAuth::Strategies::OAuth2
+
+      args %i[client_id client_secret]
+
       option :client_options, site: 'https://gitlab.com/api/v4'
 
       option :redirect_url
+
+      # When `true`, client_id and client_secret are returned in extra['raw_info'].
+      option :extra_client_id_and_client_secret, false
 
       uid { raw_info['id'].to_s }
 
@@ -24,7 +30,33 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('user').parsed
+        @raw_info ||= begin
+          user = access_token.get('user').parsed
+          options[:extra_client_id_and_client_secret] ? { client_id: smart_client_id, client_secret: smart_client_secret, user: user } : { user: user }
+        end
+      end
+
+      def smart_client_id
+        @smart_client_id ||= env['omniauth.params']['client_id'] || env['omniauth.strategy'].options.client_id
+      end
+
+      def smart_client_secret
+        @smart_client_secret ||= env['omniauth.params']['client_secret'] || env['omniauth.strategy'].options.client_secret
+      end
+
+      def build_access_token
+        verifier = request.params["code"]
+        # Override regular client when using setup: proc
+        if env['omniauth.params']['client_id'] && env['omniauth.params']['client_secret'] && env['omniauth.params']['site']
+          client = ::OAuth2::Client.new(
+            env['omniauth.params']['client_id'],
+            env['omniauth.params']['client_secret'],
+            site: env['omniauth.params']['site'],
+          )
+          client.auth_code.get_token(verifier, {:redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
+        else
+          super
+        end
       end
 
       private
